c83ee15fdd78b6ee49457f1d715237b60c26ae25
Johab Freitas <freitasjohab@gmail.com>
2026-05-26 11:26:05 -0300

    refactor: modificando o tipo da variável de Wrapper para Primitivo

--- src/main/java/com/desafio100diasjava/oneday/Exercicio2.java
+++ src/main/java/com/desafio100diasjava/oneday/Exercicio2.java
@@ -8,8 +8,8 @@
         public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
 
-           Double numero1;
-           Double numero2;
+           double numero1;
+           double numero2;
 
            System.out.println("Número 1: ");
            numero1 = sc.nextDouble();
@@ -18,19 +18,19 @@
            numero2 = sc.nextDouble();
 
            System.out.println("------SOMA------");
-           Double resultadoSoma = numero1 + numero2;
+           double resultadoSoma = numero1 + numero2;
            System.out.println(String.format("A soma é: %.0f", resultadoSoma));
 
            System.out.println("------SUBTRAÇÃO------");
-           Double resultadoSub = numero1 - numero2;
+           double resultadoSub = numero1 - numero2;
            System.out.println(String.format("A subtração é: %.0f", resultadoSub));
 
            System.out.println("------MULTIPLICAÇÃO------");
-           Double resultadoMulti = numero1 * numero2;
+           double resultadoMulti = numero1 * numero2;
            System.out.println(String.format("A multiplicação é: %.0f", resultadoMulti));
 
            System.out.println("------DIVISÃO------");
-           Double resultadoDiv = numero1 / numero2;
+           double resultadoDiv = numero1 / numero2;
            System.out.println(String.format("A divisão é: %.0f", resultadoDiv));
 
            sc.close();
