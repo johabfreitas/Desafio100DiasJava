507c80e10f960c804c5a74d4eda0ca1de28adb68
Johab Freitas <freitasjohab@gmail.com>
2026-05-26 11:22:06 -0300

    feat: implementado o resultado final do imc.

--- src/main/java/com/desafio100diasjava/oneday/Exercicio3.java
+++ src/main/java/com/desafio100diasjava/oneday/Exercicio3.java
@@ -18,5 +18,15 @@ public class Exercicio3 {
         System.out.println(String.format("O IMC: %.2f", imc));
 
         sc.close();
+        System.out.println("Resultado final!");
+        if (imc < 18.5) {
+            System.out.println("Abaixo do peso");
+        } else if (imc < 25) {
+            System.out.println("Peso normal");
+        } else if (imc < 30) {
+            System.out.println("Sobrepeso");
+        } else {
+            System.out.println("Obesidade");
+        }
     }
 }
